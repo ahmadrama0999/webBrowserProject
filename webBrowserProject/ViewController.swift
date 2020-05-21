a7d3e1248df24142909b3d2b93b796965e8ae067
Akhmad Ramin <ahmadrama0999@gmail.com>
2020-05-21 14:10:29 +0300

    Initial Commit

---
+++ webBrowserProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webBrowserProject
+//
+//  Created by Ramin Akhmad on 21.05.2020.
+//  Copyright © 2020 Ramin Akhmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
