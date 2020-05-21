a95de8c3350a9f53508d6849433c11627a40158b
Akhmad Ramin <ahmadrama0999@gmail.com>
2020-05-21 15:41:09 +0300

    initial commit + task done

--- webBrowserProject/ViewController.swift
+++ webBrowserProject/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKUIDelegate {
+    
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBAction func searchAction(_ sender: Any) {
+        guard let url = searchTextField.text else { return }
+        let request = URLRequest( url: URL(string: url)!)
+        webView.load(request)
+    }
+    
+    @IBAction func goBackAction(_ sender: Any) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func goForwardAction(_ sender: Any) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
